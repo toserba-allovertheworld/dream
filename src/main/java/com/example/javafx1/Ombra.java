a0b787ca93968c2dc1afc9f03a4a71d971b6d9ff
matteo.duso <matteo.duso@itiszuccante.edu.it>
2026-05-05 14:10:11 +0200

    PER COLOMBO

--- src/main/java/com/example/javafx1/Ombra.java
+++ src/main/java/com/example/javafx1/Ombra.java
@@ -1,4 +1,7 @@
 package com.example.javafx1;
 
 public class Ombra extends Nemico{
+    public Ombra(double x, double y, double dimensionX, double dimensionY) {
+        super(x, y, dimensionX, dimensionY);
+    }
 }
